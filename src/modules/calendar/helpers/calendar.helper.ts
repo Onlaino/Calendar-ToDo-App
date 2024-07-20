a5a009dae2beb07760586fec96429d14b8904336
Oleg Vasilev <v.oleg2908@gmail.com>
2024-07-20 19:26:50 +0300

    Добавить enum для дней недели и месяцев

--- src/modules/calendar/helpers/calendar.helper.ts
+++ src/modules/calendar/helpers/calendar.helper.ts
@@ -1,16 +1,49 @@
-export const months = [
-	'Январь',
-	'Февраль',
-	'Март',
-	'Апрель',
-	'Май',
-	'Июнь',
-	'Июль',
-	'Август',
-	'Сентябрь',
-	'Октябрь',
-	'Ноябрь',
-	'Декабрь',
-]
+enum MonthEnum {
+	January = 'Январь',
+	February = 'Февраль',
+	March = 'Март',
+	April = 'Апрель',
+	May = 'Май',
+	June = 'Июнь',
+	July = 'Июль',
+	August = 'Август',
+	September = 'Сентябрь',
+	October = 'Октябрь',
+	November = 'Ноябрь',
+	December = 'Декабрь',
+}
 
-export const weekdays = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс']
+export const months = [
+	MonthEnum.January,
+	MonthEnum.February,
+	MonthEnum.March,
+	MonthEnum.April,
+	MonthEnum.May,
+	MonthEnum.June,
+	MonthEnum.July,
+	MonthEnum.August,
+	MonthEnum.September,
+	MonthEnum.October,
+	MonthEnum.November,
+	MonthEnum.December,
+];
+
+enum WeekdaysEnum {
+	Monday = 'Пн',
+	Tuesday = 'Вт',
+	Wednesday = 'Ср',
+	Thursday = 'Чт',
+	Friday = 'Пт',
+	Saturday = 'Сб',
+	Sunday = 'Вс',
+}
+
+export const weekdays = [
+	WeekdaysEnum.Monday,
+	WeekdaysEnum.Tuesday,
+	WeekdaysEnum.Wednesday,
+	WeekdaysEnum.Thursday,
+	WeekdaysEnum.Friday,
+	WeekdaysEnum.Saturday,
+	WeekdaysEnum.Sunday,
+];
